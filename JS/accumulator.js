d05d53601a85c432fc66a6db81d975fe81eb7090
Gregoire Gay <gregoire.gay82@gmail.com>
2020-10-26 15:11:16 +0100

    test commit

---
+++ JS/accumulator.js
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html>
+<head data-suburl="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	
+		<meta name="author" content="rturpault" />
+		<meta name="description" content="relecture_js_avance - JS brouillon " />
+		<meta name="keywords" content="go, git, self-hosted, gogs">
+	
+	<meta name="referrer" content="no-referrer" />
+	<meta name="_csrf" content="R5FWc_oLCcexefs2kTRmuCmpst86MTYwMzcwOTc3NDE1NTk3OTQ1Nw==" />
+	<meta name="_suburl" content="" />
+	
+	
+	
+		<meta property="og:url" content="https://cours.3wa.fr/rturpault/relecture_js_avance" />
+		<meta property="og:type" content="object" />
+		<meta property="og:title" content="rturpault/relecture_js_avance">
+		<meta property="og:description" content="JS brouillon ">
+		<meta property="og:image" content="https://secure.gravatar.com/avatar/d5dee5d4196ce9a4480cf2c023297f17?d=identicon" />
+	
+
+	<link rel="shortcut icon" href="/img/favicon.png" />
+
+	<script src="/js/jquery-1.11.3.min.js"></script>
+	<script src="/js/libs/jquery.are-you-sure.js"></script>
+	<link rel="stylesheet" href="/assets/font-awesome-4.6.3/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/assets/octicons-4.3.0/octicons.min.css">
+
+	
+	
+
+	
+
+	
+	<link rel="stylesheet" href="/css/semantic-2.3.1.min.css">
+	<link rel="stylesheet" href="/css/gogs.css?v=90aea85ecc4109281906dd1214da777f">
+	<noscript>
+		<style>
+			.dropdown:hover > .menu { display: block; }
+			.ui.secondary.menu .dropdown.item > .menu { margin-top: 0; }
+		 </style>
+	</noscript>
+
+	
+	<script src="/js/semantic-2.3.1.min.js"></script>
+	<script src="/js/gogs.js?v=90aea85ecc4109281906dd1214da777f"></script>
+
+	<title>rturpault/relecture_js_avance: JS brouillon  - Cours 3W Academy</title>
+
+	<meta name="theme-color" content="#ff5343">
+
+	
+</head>
+<body>
+	<div class="full height">
+		<noscript>This website works better with JavaScript</noscript>
+
+		
+			<div class="following bar light">
+				<div class="ui container">
+					<div class="ui grid">
+						<div class="column">
+							<div class="ui top secondary menu">
+								<a class="item brand" href="/">
+									<img class="ui mini image" src="/img/favicon.png">
+								</a>
+
+								
+									<a class="item" href="/">Tableau de bord</a>
+									<a class="item" href="/issues">Tickets</a>
+									<a class="item" href="/pulls">Pull Requests</a>
+								
+
+								<a class="item" href="/explore/repos">Explorer</a>
+								
+
+								
+									<div class="right menu">
+										<div class="ui dropdown head link jump item poping up" data-content="Créer..." data-variation="tiny inverted">
+											<span class="text">
+												<i class="octicon octicon-plus"><span class="sr-only">Créer...</span></i>
+												<i class="octicon octicon-triangle-down"></i>
+											</span>
+											<div class="menu">
+												<a class="item" href="/repo/create">
+													<i class="octicon octicon-plus"></i> Nouveau dépôt
+												</a>
+												<a class="item" href="/repo/migrate">
+													<i class="octicon octicon-repo-clone"></i> Nouvelle migration
+												</a>
+												
+											</div>
+										</div>
+
+										<div class="ui dropdown head link jump item poping up" tabindex="-1" data-content="Profil utilisateur et plus" data-variation="tiny inverted">
+											<span class="text avatar">
+												<img class="ui small rounded image" src="https://secure.gravatar.com/avatar/ee41d71fb2c24a3314229b9f0f87ad1e?d=identicon">
+												<span class="sr-only">Profil utilisateur et plus</span>
+												<i class="octicon octicon-triangle-down" tabindex="-1"></i>
+											</span>
+											<div class="menu" tabindex="-1">
+												<div class="ui header">
+													Connecté en tant que <strong>gregoire-gay</strong>
+												</div>
+
+												<div class="divider"></div>
+												<a class="item" href="/gregoire-gay">
+													<i class="octicon octicon-person"></i> Votre profil
+												</a>
+												<a class=" item" href="/user/settings">
+													<i class="octicon octicon-settings"></i> Vos paramètres
+												</a>
+												<a class="item" target="_blank" rel="noopener noreferrer" href="https://gogs.io/docs" rel="noreferrer">
+													<i class="octicon octicon-question"></i> Aide
+												</a>
+												
+
+												<div class="divider"></div>
+												<a class="item" href="/user/logout">
+													<i class="octicon octicon-sign-out"></i> Déconnexion
+												</a>
+											</div>
+										</div>
+									</div>
+
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+
+
+<div class="repository file list">
+	<div class="header-wrapper">
+
+	<div class="ui container">
+		<div class="ui vertically padded grid head">
+			<div class="column">
+				<div class="ui header">
+					<div class="ui huge breadcrumb">
+						
+							<i class="mega-octicon octicon-repo-forked"></i>
+						
+						<a href="/rturpault">rturpault</a>
+						<div class="divider"> / </div>
+						<a href="/rturpault/relecture_js_avance">relecture_js_avance</a>
+						
+						<div class="fork-flag">forké depuis <a href="/a.lucsko/js_avance">a.lucsko/js_avance</a></div>
+					</div>
+
+					
+						<div class="ui right">
+							<div class="ui labeled button" tabindex="0">
+								<a class="ui basic button" href="/rturpault/relecture_js_avance/action/unwatch?redirect_to=%2frturpault%2frelecture_js_avance%2fsrc%2fmaster%2fpart1_introduction%2fExercices%2faccumulator.js">
+									<i class="eye icon"></i>Ne plus suivre
+								</a>
+								<a class="ui basic label" href="/rturpault/relecture_js_avance/watchers">
+									2
+								</a>
+							</div>
+							<div class="ui labeled button" tabindex="0">
+								<a class="ui basic button" href="/rturpault/relecture_js_avance/action/star?redirect_to=%2frturpault%2frelecture_js_avance%2fsrc%2fmaster%2fpart1_introduction%2fExercices%2faccumulator.js">
+									<i class="star outline icon"></i>Voter
+								</a>
+								<a class="ui basic label" href="/rturpault/relecture_js_avance/stars">
+									0
+								</a>
+							</div>
+							
+								<div class="ui labeled button" tabindex="0">
+									<a class="ui basic button " href="/repo/fork/88">
+										<i class="octicon octicon-repo-forked"></i>Fork
+									</a>
+									<a class="ui basic label" href="/rturpault/relecture_js_avance/forks">
+										0
+									</a>
+								</div>
+							
+						</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="ui tabs container">
+		<div class="ui tabular menu navbar">
+			
+				<a class="active item" href="/rturpault/relecture_js_avance">
+					<i class="octicon octicon-file-text"></i> Fichiers
+				</a>
+			
+			
+			
+			
+			
+		</div>
+	</div>
+	<div class="ui tabs divider"></div>
+
+</div>
+
+	<div class="ui container">
+		
+
+
+
+
+		
+		<div class="ui secondary menu">
+			
+			<div class="fitted item choose reference">
+	<div class="ui floating filter dropdown" data-no-results="Aucun résultat trouvé.">
+			<div class="ui basic small button">
+			<span class="text">
+				<i class="octicon octicon-git-branch"></i>
+				Branche:
+				<strong>master</strong>
+			</span>
+			<i class="dropdown icon"></i>
+		</div>
+		<div class="menu">
+			<div class="ui icon search input">
+				<i class="filter icon"></i>
+				<input name="search" placeholder="Filtrer une branche ou un tag...">
+			</div>
+			<div class="header">
+				<div class="ui grid">
+					<div class="two column row">
+						<a class="reference column" href="#" data-target="#branch-list">
+							<span class="text black">
+								Branches
+							</span>
+						</a>
+						<a class="reference column" href="#" data-target="#tag-list">
+							<span class="text ">
+								Tags
+							</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div id="branch-list" class="scrolling menu" >
+				
+					<div class="item selected" data-url="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js">master</div>
+				
+			</div>
+			<div id="tag-list" class="scrolling menu" style="display: none">
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+			<div class="fitted item">
+				<div class="ui breadcrumb">
+					<a class="section" href="/rturpault/relecture_js_avance/src/master">relecture_js...</a>
+					
+					
+					
+						<div class="divider"> / </div>
+						
+							
+							<span class="section"><a href="/rturpault/relecture_js_avance/src/master/part1_introduction">part1_introduction</a></span>
+						
+					
+						<div class="divider"> / </div>
+						
+							
+							<span class="section"><a href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices">Exercices</a></span>
+						
+					
+						<div class="divider"> / </div>
+						
+							<span class="active section">accumulator.js</span>
+						
+					
+				</div>
+			</div>
+			<div class="right fitted item">
+				
+					<div id="file-buttons" class="ui tiny blue buttons">
+						
+						
+					</div>
+				
+
+				
+				
+			</div>
+		</div>
+		
+			<div id="file-content" class="tab-size-8">
+	<h4 class="ui top attached header" id="repo-read-file">
+		
+			<i class="octicon octicon-file-text ui left"></i>
+			<strong>accumulator.js</strong> <span class="text grey normal">282 B</span>
+		
+		
+			<div class="ui right file-actions">
+				<div class="ui buttons">
+					
+						<a class="ui button" href="/rturpault/relecture_js_avance/src/7fbe7616204179251b61393dfcc8e79b58dbf598/part1_introduction/Exercices/accumulator.js">Lien permanent</a>
+					
+					<a class="ui button" href="/rturpault/relecture_js_avance/commits/master/part1_introduction/Exercices/accumulator.js">Historique</a>
+					<a class="ui button" href="/rturpault/relecture_js_avance/raw/master/part1_introduction/Exercices/accumulator.js">Raw</a>
+				</div>
+				
+					
+						<i class="octicon octicon-pencil btn-octicon poping up disabled" data-content="Vous devez fourcher ce dépôt avant de modifier le fichier" data-position="bottom center" data-variation="tiny inverted"></i>
+					
+					
+						<i class="octicon octicon-trashcan btn-octicon poping up disabled" data-content="Vous devez avoir un accès en écriture pour appliquer ou proposer des modifications à ce fichier" data-position="bottom center" data-variation="tiny inverted"></i>
+					
+				
+			</div>
+		
+	</h4>
+	<div class="ui unstackable attached table segment">
+		<div id="" class="file-view code-view has-emoji">
+			
+				<table>
+					<tbody>
+						<tr>
+						
+							<td class="lines-num"><span id="L1">1</span><span id="L2">2</span><span id="L3">3</span><span id="L4">4</span><span id="L5">5</span><span id="L6">6</span><span id="L7">7</span><span id="L8">8</span><span id="L9">9</span><span id="L10">10</span><span id="L11">11</span><span id="L12">12</span><span id="L13">13</span><span id="L14">14</span><span id="L15">15</span></td>
+							<td class="lines-code"><pre><code class="js"><ol class="linenums"><li class="L1" rel="L1"></li>
+<li class="L2" rel="L2"></li>
+<li class="L3" rel="L3">function accumulator(numbers, acc = 0) {</li>
+<li class="L4" rel="L4">    if (numbers.length === 0) return acc;</li>
+<li class="L5" rel="L5"></li>
+<li class="L6" rel="L6">    let curr = numbers.shift();</li>
+<li class="L7" rel="L7">    acc = curr + acc ;</li>
+<li class="L8" rel="L8"></li>
+<li class="L9" rel="L9">    return accumulator(numbers, acc)</li>
+<li class="L10" rel="L10">}</li>
+<li class="L11" rel="L11"></li>
+<li class="L12" rel="L12">let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]</li>
+<li class="L13" rel="L13">let total = accumulator(numbers);</li>
+<li class="L14" rel="L14"></li>
+<li class="L15" rel="L15">console.log(total)</li>
+</ol></code></pre></td>
+						
+						</tr>
+					</tbody>
+				</table>
+			
+		</div>
+	</div>
+</div>
+
+<script>
+function submitDeleteForm() {
+    var message = prompt("delete_confirm_message\n\ndelete_commit_summary", "Delete ''");
+    if (message != null) {
+        $("#delete-message").val(message);
+        $("#delete-file-form").submit()
+    }
+}
+</script>
+
+		
+	</div>
+</div>
+
+	</div>
+	<footer>
+		<div class="ui container">
+			<div class="ui left">
+				© 2018 Gogs Version: 0.11.63.0817 Page: <strong>92ms</strong> Modèle: <strong>3ms</strong>
+			</div>
+			<div class="ui right links">
+				
+				<div class="ui language bottom floating slide up dropdown link item">
+					<i class="world icon"></i>
+					<div class="text">français</div>
+					<div class="menu">
+						
+							<a class="item active selected" href="#">français</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=en-US">English</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=zh-CN">简体中文</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=zh-HK">繁體中文（香港）</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=zh-TW">繁體中文（臺灣）</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=de-DE">Deutsch</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=nl-NL">Nederlands</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=lv-LV">latviešu</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=ru-RU">русский</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=ja-JP">日本語</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=es-ES">español</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=pt-BR">português do Brasil</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=pl-PL">polski</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=bg-BG">български</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=it-IT">italiano</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=fi-FI">suomi</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=tr-TR">Türkçe</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=cs-CZ">čeština</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=sr-SP">српски</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=sv-SE">svenska</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=ko-KR">한국어</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=gl-ES">galego</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=uk-UA">українська</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=en-GB">English (United Kingdom)</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=hu-HU">Magyar</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=sk-SK">Slovenčina</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=id-ID">Indonesian</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=fa-IR">Persian</a>
+						
+							<a class="item " href="/rturpault/relecture_js_avance/src/master/part1_introduction/Exercices/accumulator.js?lang=vi-VN">Vietnamese</a>
+						
+					</div>
+				</div>
+				<a href="/assets/librejs/librejs.html" style="display:none" data-jslicense="1">Javascript Licenses</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://gogs.io">Site web</a>
+				<span class="version">Go1.11</span>
+			</div>
+		</div>
+	</footer>
+</body>
+
+
+
+	<link rel="stylesheet" href="/plugins/highlight-9.6.0/github.css">
+	<script src="/plugins/highlight-9.6.0/highlight.pack.js"></script>
+
+
+
+
+
+<script src="/js/libs/emojify-1.1.0.min.js"></script>
+<script src="/js/libs/clipboard-1.5.9.min.js"></script>
+
+
+</html>
+
